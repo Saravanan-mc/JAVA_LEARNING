7f105bde5875d1e4c6803eb7992463f347ca769a
saravanan-mc <saravananmcms@gmail.com>
2025-09-16 10:04:22 +0530

    print

---
+++ 1.PRINT/formatted.java
@@ -0,0 +1,7 @@
+public class Formatted{
+    public static void main(String[] args) {
+        int age = 25;
+        String name = "John";
+        System.out.printf("Name: %s, Age: %d%n", name, age);
+    }
+}
