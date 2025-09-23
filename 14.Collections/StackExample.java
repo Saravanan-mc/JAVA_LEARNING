6f72541244d52d06ca97be325aacd7c7d8b29413
saravanan-mc <saravananmcms@gmail.com>
2025-09-23 22:29:35 +0530

    stack2

--- 14.Collections/StackExample.java
+++ 14.Collections/StackExample.java
@@ -10,7 +10,19 @@ public class StackExample {
         System.out.println(s);
 
         System.out.println(s.peek());
+        System.out.println(s.pop());
+        System.out.println(s);
 
-        
+        System.out.println(s.search("a"));
+
+        System.out.println(s.contains("b"));
+
+        System.out.println(s.size());
+
+        System.out.println(s.isEmpty());
+
+        s.clear();
+        System.out.println(s);
+
     }
 }
